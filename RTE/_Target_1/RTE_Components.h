1e632b55c3ece7fa586cfe040f0fa6ec50a3598c
makarkinpawel2095dsgn <133103957+makarkinpawel2095dsgn@users.noreply.github.com>
2023-05-19 18:02:42 +0300

    Add files via upload

---
+++ RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,37 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'adc_uart' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "apm32f10x.h"
+
+/* Geehy::Device:StdPeriph Drivers:DMA:1.0.2 */
+#define RTE_DEVICE_STDPERIPH_DMA
+/* Geehy::Device:StdPeriph Drivers:EINT:1.0.2 */
+#define RTE_DEVICE_STDPERIPH_EINT
+/* Geehy::Device:StdPeriph Drivers:Framework:1.0.2 */
+#define RTE_DEVICE_STDPERIPH_FRAMEWORK
+/* Geehy::Device:StdPeriph Drivers:GPIO:1.0.2 */
+#define RTE_DEVICE_STDPERIPH_GPIO
+/* Geehy::Device:StdPeriph Drivers:RCM:1.0.2 */
+#define RTE_DEVICE_STDPERIPH_RCM
+/* Geehy::Device:StdPeriph Drivers:SPI:1.0.2 */
+#define RTE_DEVICE_STDPERIPH_SPI
+/* Geehy::Device:StdPeriph Drivers:TMR:1.0.2 */
+#define RTE_DEVICE_STDPERIPH_TMR
+/* Geehy::Device:StdPeriph Drivers:USART:1.0.2 */
+#define RTE_DEVICE_STDPERIPH_USART
+
+
+#endif /* RTE_COMPONENTS_H */
