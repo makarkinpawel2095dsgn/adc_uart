6e2fd061f861b4436d9a13f78c896f9be3ce0ade
makarkinpawel2095dsgn <133103957+makarkinpawel2095dsgn@users.noreply.github.com>
2023-05-19 18:01:29 +0300

    Add files via upload

---
+++ Objects/apm32f10x_misc.d
@@ -0,0 +1,10 @@
+./objects/apm32f10x_misc.o: \
+  C:\Users\makp3\AppData\Local\Arm\Packs\Geehy\APM32F1xx_DFP\1.0.8\Device\StdPeriphDriver\src\apm32f10x_misc.c \
+  C:\Users\makp3\AppData\Local\Arm\Packs\Geehy\APM32F1xx_DFP\1.0.8\Device\StdPeriphDriver\inc\apm32f10x_misc.h \
+  C:\Users\makp3\AppData\Local\Arm\Packs\Geehy\APM32F1xx_DFP\1.0.8\Device\Device\Geehy\APM32F10x\Include\apm32f10x.h \
+  C:\Users\makp3\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h \
+  C:\tools\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\makp3\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\makp3\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\makp3\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\makp3\AppData\Local\Arm\Packs\Geehy\APM32F1xx_DFP\1.0.8\Device\Device\Geehy\APM32F10x\Include\system_apm32f10x.h
---
+++ Objects/main.d
@@ -0,0 +1,14 @@
+./objects/main.o: main.c \
+  C:\Users\makp3\AppData\Local\Arm\Packs\Geehy\APM32F1xx_DFP\1.0.8\Device\Device\Geehy\APM32F10x\Include\apm32f10x.h \
+  C:\Users\makp3\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h \
+  C:\tools\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  C:\Users\makp3\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
+  C:\Users\makp3\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
+  C:\Users\makp3\AppData\Local\Arm\Packs\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
+  C:\Users\makp3\AppData\Local\Arm\Packs\Geehy\APM32F1xx_DFP\1.0.8\Device\Device\Geehy\APM32F10x\Include\system_apm32f10x.h \
+  C:\Users\makp3\AppData\Local\Arm\Packs\Geehy\APM32F1xx_DFP\1.0.8\Device\StdPeriphDriver\inc\apm32f10x_gpio.h \
+  C:\Users\makp3\AppData\Local\Arm\Packs\Geehy\APM32F1xx_DFP\1.0.8\Device\StdPeriphDriver\inc\apm32f10x_rcm.h \
+  C:\Users\makp3\AppData\Local\Arm\Packs\Geehy\APM32F1xx_DFP\1.0.8\Device\StdPeriphDriver\inc\apm32f10x_usart.h \
+  ADS1282.h \
+  C:\Users\makp3\AppData\Local\Arm\Packs\Geehy\APM32F1xx_DFP\1.0.8\Device\StdPeriphDriver\inc\apm32f10x_spi.h \
+  tim_adc.h
